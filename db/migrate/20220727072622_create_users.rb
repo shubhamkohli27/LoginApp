3f3a964d7e646def8ea7e47ea6c17b36463bd68e
shubham kohli <shubham.kohli@go-yubi.com>
2022-08-01 17:30:49 +0530

    Login APP

---
+++ db/migrate/20220727072622_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  validates :login, :email, presence: true
+   
+
+  def change
+    create_table :users do |t|
+      t.string :email , null: false
+      #Ex:- :null => false
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
